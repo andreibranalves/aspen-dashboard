00a1179c14a5d78486f0c8347e5251de2a40804d
Andrei Brandão Alves <andrei.bran.alves@gmail.com>
2026-06-16 23:10:00 -0300

    sidebar: sempre dark mode independente do tema global

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -80,7 +80,7 @@ export default function Sidebar({
           {!collapsed && (
             <div className="flex items-center gap-2.5 whitespace-nowrap">
               <img
-                src={darkMode ? '/logo_branca.svg' : '/logo_marinho.svg'}
+                src="/logo_branca.svg"
                 alt="Aspen Estamparia"
                 style={{ height: '2rem', width: 'auto' }}
               />
