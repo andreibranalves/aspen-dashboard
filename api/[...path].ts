ec2033e428dfbd155c262185f1657a163c3da3bb
Andrei Brandão Alves <andrei.bran.alves@gmail.com>
2026-09-23 10:14:37 -0300

    feat: add privacy-safe Sentry error monitoring

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,4 @@
+import './_infrastructure/integrations/sentry/client.js';
 import { handleApiRequest } from './_app/handle-request.js';
 import { createVercelHandler } from './_http/vercel-adapter.js';
 
